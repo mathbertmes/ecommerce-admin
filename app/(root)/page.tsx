143cc5bca940c1356f5b112877288513d3d50dcc
Matheus Bertemes <mathbertemes@gmail.com>
2024-01-10 02:11:57 -0800

    auth fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button>Click me</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
+    
   )
 }
+
+export default SetupPage;
