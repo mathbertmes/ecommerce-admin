ce09a9124fcb3ede0f373128d44953bea5078093
Matheus Bertemes <mathbertemes@gmail.com>
2024-01-27 23:39:03 -0800

    main-nav routes links

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils"
+import Link from "next/link"
 import { useParams, usePathname } from "next/navigation"
 
 export function MainNav({
@@ -9,11 +10,31 @@ export function MainNav({
 }: React.HTMLAttributes<HTMLElement>){
   const pathname = usePathname()
   const params = useParams()
-  return(
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: 'Settings',
+      active: pathname === `/${params.storeId}/settings`
+    }
+  ]
+
+  return(    
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
     >
-
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active ? "text-black dark:text-white" : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
     </nav>
   )
 }
