4e12c7a4f9abf662e7e9323ea6e7fc0b54bebf54
Matheus Bertemes <mathbertemes@gmail.com>
2024-06-08 02:00:57 -0700

    fear: get categories with discount

---
+++ app/api/[storeId]/categories-discount/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server"
+import prismadb from "@/lib/prismadb"
+
+
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+
+){
+  try {
+
+    if(!params.storeId){
+      return new NextResponse("Store ID is required", {status : 400})
+    }
+
+    const categories = await prismadb.category.findMany({
+      where: {
+          products: {
+              some: {
+                  discount: true,
+                  isArchived: false,
+              }
+          }
+      }
+  });
+
+    return NextResponse.json(categories)
+
+  } catch(error) {
+    console.log("[CATEGORIES-DISCOUNT_GET]", error)
+    return new NextResponse("Internal Error", {status : 500})
+  }
+}
