9ebad790b99b6d86bc6d58c2e8dbf4a2a3f0d153
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-03 22:43:02 -0700

    refactor: total revenue updated

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -4,12 +4,17 @@ export const getTotalRevenue = async (storeId: string) => {
   const paidOrders = await prismadb.order.findMany({ 
     where: {
       storeId,
-      isPaid: true,
     },
     include: {
       orderItems: {
         include: {
-          product: true
+          sizeStock : {
+            include: {
+              product: true
+            }
+            
+          }
+          
         }
       }
     }
@@ -17,7 +22,7 @@ export const getTotalRevenue = async (storeId: string) => {
 
   const totalRevenue = paidOrders.reduce((total, order) => {
     const orderTotal = order.orderItems.reduce((orderSum, item) => {
-      return orderSum + item.product.price.toNumber()
+      return orderSum + item.itemPrice.toNumber()
     }, 0)
 
     return total + orderTotal
