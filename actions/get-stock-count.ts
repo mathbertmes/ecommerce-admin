506ef2aca161fd080b50b52c45807afd38252bad
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-03 22:43:15 -0700

    refactor: total stock updated

--- actions/get-stock-count.ts
+++ actions/get-stock-count.ts
@@ -1,12 +1,26 @@
 import prismadb from "@/lib/prismadb";
 
 export const getStockCount = async (storeId: string) => {
-  const stockCount = await prismadb.product.count({ 
+  const stockCount = await prismadb.product.findMany({ 
     where: {
       storeId,
       isArchived: false
+    },
+    include: {
+      stock : true
     }
   })
 
-  return stockCount;
+  const test = stockCount.map((product) => {
+    let itemStock = product.stock.reduce((stockSum, item) => {
+      return stockSum + item.amount
+    }, 0)
+    return itemStock
+  })
+
+  const finalStock = test.reduce((sum, itemStock) => {
+    return sum + itemStock
+  }, 0)
+
+  return finalStock;
 }
