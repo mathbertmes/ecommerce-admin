4ea15ef3f0a99941d48372d1b0d8891cd32e5bd1
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-12 23:44:32 -0700

    feat: size stock client calling modal

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/stock/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/stock/components/client.tsx
@@ -1,26 +1,41 @@
 "use client"
 
+import { SizeStockModal } from "@/components/modals/update-size-modal";
 import { Button } from "@/components/ui/button";
 import { Product, SizeStock } from "@prisma/client"
+import { useState } from "react";
 
 interface SizeStockClientProps{
   data : SizeStock[] | null
+  product: Product | null
 }
 
 const SizeStockClient: React.FC<SizeStockClientProps> = ({
-  data
+  data,
+  product
 }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [modalData, setModalData] = useState(null)
+
   return(
-    <div>
+    <>
+    <SizeStockModal
+      isOpen={modalIsOpen}
+      onClose={() => setModalIsOpen(false)}
+      initialData={modalData}
+    />
       <div>
-            {data?.length ?<h1>ola</h1> : <h1>This product doesnt have any stock yet</h1>}
-          </div>
-          <div>
-            <Button variant="default" className="bg-green-600">
-              Create new size
-            </Button>
-          </div>
-    </div>
+        <div>
+              {data?.length ?<h1>ola</h1> : <h1>This product doesnt have any stock yet</h1>}
+            </div>
+            <div>
+              <Button variant="default" onClick={() => setModalIsOpen(true)} className="bg-green-600">
+                Create new size
+              </Button>
+            </div>
+      </div>
+    </>
   )
 }
 
