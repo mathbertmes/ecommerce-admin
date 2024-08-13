5da6687de6201a21f4baddee47bd70faff0deccd
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-13 16:42:48 -0700

    feat: stock page client

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/stock/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/stock/components/client.tsx
@@ -1,42 +1,61 @@
-"use client"
+"use client";
 
 import { SizeStockModal } from "@/components/modals/update-size-modal";
 import { Button } from "@/components/ui/button";
-import { Product, SizeStock } from "@prisma/client"
+import { Product, SizeStock } from "@prisma/client";
 import { useState } from "react";
 
-interface SizeStockClientProps{
-  data : SizeStock[] | null
-  product: Product | null
+interface SizeStockClientProps {
+  data: SizeStock[] | null;
+  product: Product | null;
 }
 
-const SizeStockClient: React.FC<SizeStockClientProps> = ({
-  data,
-  product
-}) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [modalData, setModalData] = useState(null)
+const SizeStockClient: React.FC<SizeStockClientProps> = ({ data, product }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [modalData, setModalData] = useState(null);
 
-  return(
+  const handleOnClose = () => {
+    setModalIsOpen(false)
+    setModalData(null)
+  }
+
+  return (
     <>
-    <SizeStockModal
-      isOpen={modalIsOpen}
-      onClose={() => setModalIsOpen(false)}
-      initialData={modalData}
-    />
+      <SizeStockModal
+        isOpen={modalIsOpen}
+        onClose={handleOnClose}
+        initialData={modalData}
+      />
       <div>
         <div>
-              {data?.length ?<h1>ola</h1> : <h1>This product doesnt have any stock yet</h1>}
-            </div>
-            <div>
-              <Button variant="default" onClick={() => setModalIsOpen(true)} className="bg-green-600">
-                Create new size
-              </Button>
-            </div>
+          {data?.length ? (
+            <>
+              {data?.map((item) => (
+                <div key={item.id}>
+                  <div>
+                    <h3>Size {item.value}</h3>
+                    <h3>Amount {item.amount}</h3>
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <h1>This product doesnt have any stock yet</h1>
+          )}
+        </div>
+        <div>
+          <Button
+            variant='default'
+            onClick={() => setModalIsOpen(true)}
+            className='bg-green-600'
+          >
+            Create new size
+          </Button>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SizeStockClient
+export default SizeStockClient;
