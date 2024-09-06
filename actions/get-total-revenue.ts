043df988a5174a3318f14192194a7584bd0bbcd3
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-06 11:36:07 -0700

    fix: total revenue

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -6,17 +6,7 @@ export const getTotalRevenue = async (storeId: string) => {
       storeId,
     },
     include: {
-      orderItems: {
-        include: {
-          sizeStock : {
-            include: {
-              product: true
-            }
-            
-          }
-          
-        }
-      }
+      orderItems: true
     }
   })
 
