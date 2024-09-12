6f98c605d890a6a9c43e0a41f67a338b97343624
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-12 13:00:26 -0700

    fix: colums stock

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/stock/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/stock/components/columns.tsx
@@ -9,7 +9,7 @@ export type SizeStockColumn = {
   id: string;
   value: string;
   amount: number;
-  orderItems: OrderItem[]
+
 }
 
 export const columns: ColumnDef<SizeStockColumn>[] = [
@@ -20,11 +20,6 @@ export const columns: ColumnDef<SizeStockColumn>[] = [
   {
     accessorKey: "amount",
     header: "Units"
-  },
-  {
-  accessorKey: "orderItems",
-    header: "Orders",
-    cell: ({ row }) => row.original.orderItems.length
   },
   {
     id: "actions",
