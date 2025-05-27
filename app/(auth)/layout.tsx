a8cd4b447809a67402cab296fd777c40058f8860
Matheus Bertemes <mathbertemes@gmail.com>
2025-05-27 14:20:43 -0300

    FIX: DEMO LOGIN

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,15 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex items-center justify-center h-full w-full">
-      {children}
+      <div className="flex items-start">
+        {children}
+        <div className="w-[400px]">
+          <h1 className="text-2xl mb-5">Demo</h1>
+          <h2 className="text-lg mb-5">Para acessar a demo utilize o seguinte email e senha:</h2>
+          <h3 className="text-lg"><strong>Email:</strong> mathbertemes_ecommerce@outlook.com</h3>
+          <h3 className="text-lg"><strong>Senha:</strong> ecommerceTestStore</h3>
+        </div>
+      </div>
     </div>
   );
 };
